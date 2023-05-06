e1001beb87923bc82a24362007aa571ee2f7a383
Ajay Chauhan <ajay9670chauhan@gmail.com>
2023-05-07 01:04:53 +0530

    one to many relationship prisma

---
+++ script.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.findMany()
+    console.log(user);
+
+}
+
+main()
+    .catch(e => {
+        console.log(e.message)
+    })
+    .finally(async () => {
+        await prisma.$connect()
+    })
